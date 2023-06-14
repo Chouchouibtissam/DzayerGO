767c0e2a7ee57fb5574c51c2b06628298caae75d
Serine Gheddou <ks_gheddou@esi.dz>
2023-06-14 12:17:59 +0100

    added firebase configuration

---
+++ lib/Services/Lieu_Service.dart
@@ -0,0 +1,20 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class LieuService{
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  late final db = FirebaseDatabase.instance.ref();
+  //Getting data from collection
+  getLieux() async {
+    List<Plant> _plantList = [];
+    QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('users').doc(user.uid).collection('Plants').get().then((value) {
+      value.docs.forEach((element) {
+        Plant plant = Plant.fromMap(element.data());
+        _plantList.add(plant);
+      });
+      return value;
+    });
+
+
+  }
+}
