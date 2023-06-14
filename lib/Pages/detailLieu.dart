5660f7a4983b6a8adf772c832dda460b9fb35735
Serine Gheddou <ks_gheddou@esi.dz>
2023-06-14 11:42:27 +0100

    Detail page added

---
+++ lib/Pages/detailLieu.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetailLieu extends StatefulWidget {
+  const DetailLieu({Key? key}):super(key:key);
+  @override
+  State<DetailLieu> createState()=>_DetailLieu();
+}
+
+class _DetailLieu extends State<DetailLieu>{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+
+
+}
